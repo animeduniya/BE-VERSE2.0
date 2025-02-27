682ab086bd1d423064ac1fe0f4a127d5fe6d7517
animeduniya <duniyabarbaad24@gmail.com>
2025-02-27 20:50:01 +0530

    Create anime-movies.js

---
+++ pages/anime-movies.js
@@ -0,0 +1,8 @@
+export default function AnimeMovies() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white p-8">
+      <h1 className="text-4xl font-bold">Anime Movies</h1>
+      <p className="mt-4">Coming soon...</p>
+    </div>
+  );
+}
